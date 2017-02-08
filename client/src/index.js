42c1e1b645d8a6138f5055cbc680e7e65e3c96aa
Anthony Accomazzo <accomazz@gmail.com>
2017-02-08 15:28:06 -0700

    Move to npm semantic

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import './semantic-ui/semantic.min.css';
+import '../semantic/dist/semantic.min.css';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'), // eslint-disable-line no-undef
 );
